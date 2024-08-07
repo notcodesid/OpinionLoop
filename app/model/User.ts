2a5dc73921976186686f133538c7558716c7bc3b
siddharth <notcodesid@gmail.com>
2024-08-08 02:14:45 +0530

    done the Schema

--- app/model/User.ts
+++ app/model/User.ts
@@ -1,69 +1,22 @@
-import mongoose, {Schema , Document} from "mongoose";
+import mongoose , {Schema} from "mongoose";
 
-export interface Message extends Document{
-    content : string,
-    createdAt : Date
-}
 
-export interface User extends Document{
-    username : string,
-    email : string ,
-    password : string,
-    verifyCode : string,
-    verifyCodeExpiry : Date,
-    isVerified : boolean
-    isAcceptingMessage : boolean
-    message : Message []
-}
-
-const MessageSchema: Schema<Message> = new Schema({
-    content : {
-        type : String,
-        required: true
-    },
-    createdAt : {
-        type : Date,
-        required : true,
-        default : Date.now
-    }
+const MessageSchema= new Schema({
+content : String,
+creartedAt : Date
 })
 
-const UserSchema : Schema<User> = new Schema ({
-username : {
-    type : String,
-    required : [true , "Username is required"],
-    trim : true,
-    unique : true
-},
-email : {
-    type :  String,
-    reuqired : [true , "Email is required"],
-    unique: true,
-    match : [/.+\@.+\..+/ , 'Use a vaild Email'],
-},
-password : {
-    type : String ,
-    required : [true , 'Password is required']
-},
-verifyCode : {
-    type : String , 
-    required : [true , 'Verify Code is required']
-},
-verifyCodeExpiry : {
-    type : Date , 
-    required : [true , 'Verify code Expiry is required']
-},
-isVerified : {
-    type : Boolean,
-    default : false
-},
-isAcceptingMessage : {
-    type : Boolean,
-    default : true
-},
-message : [MessageSchema]
-})
 
-const userModel = (mongoose.models.User as mongoose.Model<User>)|| mongoose.model<User>("User" , UserSchema)
+const userSchema = new Schema({
+  username : String,
+  email : String ,
+  password : String,
+  VerfiedCode : Boolean,
+  verifyCodeExpiry : Date,
+  isAdmin : Boolean,
+  isAcceptingMsg : Boolean,
+  message : Array
+});
 
-export default userModel;
+export const user = mongoose.model('User' ,userSchema);
+export const messgae = mongoose.model('Message' ,MessageSchema);
--- app/schemas/messgaeSchema.ts
+++
@@ -1,8 +0,0 @@
-import { z} from 'zod';
-
-export const MsgSchema = z.object({
-    content : z
-    .string()
-    .min(10 , {message : "Content must be atleast 10 character"})
-    .max(100 , {message : "Content must be no longer than  100 character"})
-})
