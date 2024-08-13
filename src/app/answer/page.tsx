805d56ae6f7dc97ea5880328dbc52952ac7a0ff7
siddharth <notcodesid@gmail.com>
2024-08-13 16:05:42 +0530

    qns card inside the ans page

--- src/app/answer/page.tsx
+++ src/app/answer/page.tsx
@@ -1,8 +1,12 @@
 "use client";
-import  QsnCard  from "../.././components/qsnCard";
 import { useQnsFromDB } from "../hooks";
 
-export default function Answer() {
+interface qsnProps{
+  qsn : string
+}
+
+
+export function Answer() {
   const { loading, post } = useQnsFromDB();
 
   if (loading) {
@@ -36,4 +40,17 @@ export default function Answer() {
       </div>
     </section>
   );
+}
+
+export default function QsnCard({qsn} : qsnProps) {
+  return (
+      <div className="relative overflow-hidden transition-transform duration-300 ease-in-out rounded-lg shadow-lg group hover:shadow-xl hover:-translate-y-2">
+      <div className="p-4 bg-background">
+        <p className="text-sm text-muted-foreground">
+          {/* Display the content here */}
+          {qsn}
+        </p>
+      </div>
+    </div>
+  )
 }
